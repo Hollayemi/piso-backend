5946dc1dd92556e59c3a3262d609fed77225ff22
Oluwasusi Stephen Olayemi <stephanyemmitty@gmail.com>
2026-09-07 13:19:41 -0700

    Update server.js
    commented the rate limiter

--- server.js
+++ server.js
@@ -36,17 +36,17 @@ app.use(cors(corsOptions));
 
 // ─── Rate Limiting ────────────────────────────────────────────────────────────
 
-const globalLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max:      100,
-    message:  'Too many requests from this IP, please try again later.',
-});
+// const globalLimiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max:      100,
+//     message:  'Too many requests from this IP, please try again later.',
+// });
 
-const submissionLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour
-    max:      10,
-    message:  'Too many request applications submitted. Please try again later.',
-});
+// const submissionLimiter = rateLimit({
+//     windowMs: 60 * 60 * 1000, // 1 hour
+//     max:      10,
+//     message:  'Too many request applications submitted. Please try again later.',
+// });
 
 app.use('/api/', globalLimiter);
 
